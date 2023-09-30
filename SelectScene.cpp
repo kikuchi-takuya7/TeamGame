e4d65bf73f84ec916ce36c950c1b367ca84ed06b
一護 武田 <220256@jc-21.jp>
2023-10-01 03:54:34 +0900

    背景表示はできたが画像が壊れてた

--- SelectScene.cpp
+++ SelectScene.cpp
@@ -1,8 +1,9 @@
 #include "SelectScene.h"
+#include "Engine/Image.h"
 
 //コンストラクタ
 SelectScene::SelectScene(GameObject* parent)
-	: GameObject(parent, "SelectScene")
+	: GameObject(parent, "SelectScene"), hPict_(-1)
 {
 }
 
@@ -14,16 +15,24 @@ SelectScene::~SelectScene()
 //初期化
 void SelectScene::Initialize()
 {
+	//画像データのロード
+	hPict_ = Image::Load("haikei.jpg");
+	assert(hPict_ >= 0);
+	
 }
 
 //更新
 void SelectScene::Update()
 {
+	SceneManager* pSceneManager = (SceneManager*)FindObject("SceneManager");
+	pSceneManager->ChangeScene(SCENE_ID_SELECT);
 }
 
 //描画
 void SelectScene::Draw()
 {
+	Image::SetTransform(hPict_, transform_);
+	Image::Draw(hPict_);
 }
 
 //開放
--- SelectScene.h
+++ SelectScene.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "Engine/GameObject.h"
+#include "Engine/SceneManager.h"
 
 class SelectScene : public GameObject
 {
+private:
+	int hPict_;    //画像番号
 public:
 	//コンストラクタ
 	//引数：parent  親オブジェクト（SceneManager）
