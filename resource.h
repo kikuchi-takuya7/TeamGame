7e21a9191e3014ae4cbf7111db2419921161599a
偉央 小野寺 <220342@jc-21.jp>
2023-10-02 11:51:55 +0900

    救出

--- resource.h
+++ resource.h
@@ -1,14 +1,19 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by GameBaseDx11.rc
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// GameBaseDx11.rc で使用
+//
+#define IDD_DIALOG1                     101
+#define IDC_YES                         1001
+#define IDC_BUTTON2                     1002
+#define IDC_NO                          1002
 
-// 新しいオブジェクトの次の既定値
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
