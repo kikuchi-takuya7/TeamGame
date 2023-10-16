a988cbc2ee30c41da046d99c40a753bb7ea00a94
彩姫 三澤 <220285@jc-21.jp>
2023-10-16 13:33:17 +0900

    画面遷移、画像貼り付け完了

--- GameClearScene.cpp
+++ GameClearScene.cpp
@@ -1,9 +1,11 @@
 #include "GameClearScene.h"
 #include "Engine/SceneManager.h"
+#include "Engine/Input.h"
+#include "Engine/Image.h"
 
 //コンストラクタ
 GameClearScene::GameClearScene(GameObject* parent)
-	: GameObject(parent, "GameClearScene")
+	: GameObject(parent, "GameClearScene"), hPict_(-1)
 {
 }
 
@@ -15,18 +17,32 @@ GameClearScene::~GameClearScene()
 //初期化
 void GameClearScene::Initialize()
 {
+	//画像データのロード
+	hPict_ = Image::Load("ClearImage.jpg");
+	assert(hPict_ >= 0);
+
+	transform_.scale_ = XMFLOAT3(1.3f, 1.53f, 1.0f);
 }
 
 //更新
 void GameClearScene::Update()
 {
-	SceneManager* pSceneManager = (SceneManager*)FindObject("SceneManager");
-	pSceneManager->ChangeScene(SCENE_ID_RESULT);
+	//スペースキーが押されていたら
+	if (Input::IsKey(DIK_SPACE))
+	{
+		SceneManager* pSceneManager = (SceneManager*)FindObject("SceneManager");
+		pSceneManager->ChangeScene(SCENE_ID_RESULT);
+	}
+
+
+	
 }
 
 //描画
 void GameClearScene::Draw()
 {
+	Image::SetTransform(hPict_, transform_);
+	Image::Draw(hPict_);
 }
 
 //開放
